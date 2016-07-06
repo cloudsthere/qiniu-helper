09207fa2bb506ad3c0edb7023601c3688f65c58d
liang <cloudsthere@163.com>
2016-07-06 16:19:30 +0800

    update

--- src/config.php
+++ src/config.php
@@ -2,17 +2,17 @@
 
 return [
 
-    'accessKey' => '0YBmkl_WFWlBrIEActy0ltskOgbc7kqRtgAofwO9',
-    'secretKey' => 'qMsDI7ZCl_8vOT_sIjmnTKtsjO7IeCOqt_2HTOn4',
+    'accessKey' => 'your_accessKey',
+    'secretKey' => 'your_secretKey',
 
     // 空间名为键，对应域名为值，第一个元素作为默认空间域名配置
     'bucket' => [
-        'test' => 'o9b6k34og.bkt.clouddn.com',
-        'space' => 'o9is2ohd6.bkt.clouddn.com'
+        'your_bucket' => 'domain of this bucket',
+        'another_bucket' => 'domain of this bucket',
     ],
 
 
-    'notify_url' => 'http://apt.niowoo.com/notify',
+    'notify_url' => 'http://notify',
 
     // 是否使用laravel的缓存
     'use_laravel_cache' => 1,
